c4466b90bb4b232682321877a1b2b64bb5403e59
MagicDK <x.co.lex@hotmail.com>
2023-09-28 16:00:51 -0300

    Basic Splash Screen Created

--- VSCode/GeographyApp/GeographyApp/MainWindow.xaml.cs
+++ VSCode/GeographyApp/GeographyApp/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Timers;
+using System.Threading;
+using System.Windows.Threading;
 
 namespace GeographyApp
 {
@@ -20,9 +23,24 @@ namespace GeographyApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        DispatcherTimer timer1 = new DispatcherTimer();
         public MainWindow()
         {
             InitializeComponent();
+            timer1.Tick += new EventHandler(timer1_Tick);
+            timer1.Interval = TimeSpan.FromMilliseconds(50);
+            timer1.Start();
         }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(MainMenuUserControls.Opacity == 1)
+            {
+                timer1.Stop();
+            }
+            MainMenuUserControls.Opacity += .1;
+        }
+
     }
+
 }

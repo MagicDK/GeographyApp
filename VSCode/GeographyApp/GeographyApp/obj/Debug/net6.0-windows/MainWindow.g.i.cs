749c128630636af677d340f4b23d084d45fe89d5
MagicDK <x.co.lex@hotmail.com>
2023-10-01 21:08:49 -0300

    Added more flags

--- VSCode/GeographyApp/GeographyApp/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ VSCode/GeographyApp/GeographyApp/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -77,7 +77,7 @@ namespace GeographyApp {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/GeographyApp;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/GeographyApp;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
